4003427bdcd3842bcc9845a97a3503c77d0c41d0
Xutong Jin <jinxutong@pku.edu.cn>
2022-09-22 14:38:22 +0800

    add fdtd test

--- tests/FDTDTest.cu
+++ tests/FDTDTest.cu
@@ -8,7 +8,7 @@ using namespace pppm;
 
 __global__ void set_center_signal(FDTD fdtd, SineSource s){
     int3 center = make_int3(fdtd.res / 2, fdtd.res / 2, fdtd.res / 2);
-    fdtd.grids[fdtd.t - 1](center) = s(fdtd.t - 1).real(); // ftdt.t is the next time step
+    fdtd.grids[fdtd.t - 1](center) = s((fdtd.t - 1) * fdtd.dt).real(); // ftdt.t is the next time step
 }
 
 int main()
@@ -17,15 +17,15 @@ int main()
 	CudaRender render;
 
     int res = 51;
-	int step_num = 200;
+	int step_num = 300;
 	float dl = 0.005;
-	float dt = 1.0f / 120000;
+	float dt = 1.0f / 150000;
 
 	GArr3D<float> data;
     data.resize(step_num, res, res);
 	FDTD fdtd;
 	fdtd.init(res, dl, dt);
-    SineSource s(500);
+    SineSource s(5000 * 2 * M_PI);
 
 	for (int i = 0; i < step_num; i++)
 	{
